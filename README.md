60e93a02f3116ecdad9ab03512ce0daecc17e5b5
Sebastian Osuna <sebastian.osuna@skaphe.com>
2014-04-20 03:27:45 -0500

    Version 1.

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ i18n
 ====
 
 Internationalization and localization library for HTML pages via Javascript.
+
+Supports nested resolving, i.e. you can use `I18N.resolve( 'a.b.c' )` to resolve 
+```
+"EN": {
+    "a": {
+        "b": {
+            "c": "Your text"
+        }
+    }
+}```
+or
+```
+"EN": { "a.b.c": "Your text" } 
+```
+
+### Requirements
+
+- [jQuery]
+
+### Usage
+
+- Add a `<script>` element after your [jQuery] import with `id="i18n"`.
+- Add a `i18n-src` attribute to the previous `<script>` tag pointing to you JSON locale file.
+- Add a `i18n-locale` attribute to the previous `<script>` tag with the locale the page will use.
+- Mark the elements that will use i18n with `i18n-text`.
+
+See the example for more details.
+
+[jQuery]:http://jquery.com
---
+++ src/i18n.js
@@ -0,0 +1,63 @@
+// !requires jquery
+var I18N = ( function() {
+	// Get source
+	var me = $( '#i18n' );
+	var default_locale = "EN";
+
+	if( me.length ) {
+		// Get I18n file
+		var src = me.attr( 'i18n-src' );
+		if( !src ) { throw new Error( 'No i18n file given.' ); }
+		// Get locale
+		var locale = me.attr( 'i18n-locale' );
+		if( !locale ) { console.log( 'No i18n locale given. Using "' + default_locale +'" as default.' ); locale = default_locale }
+		var i18n = {};
+		var full_i18n = {};
+		// Load I18N data.
+		$.get( src ).done( function( data ) {
+			full_i18n = data;
+			// Get the data from the locale
+			i18n = data[ locale ];
+			if( i18n ) {
+				// Get all elements to apply I18N
+				var elems = $( "*[i18n-text]");
+				elems.each( function( index, object ) {
+					var i18n_id = $( object ).attr( 'i18n-text' );
+					// Get value
+					var i18n_val = I18N.resolve( i18n_id );
+					// Set the HTML text
+					$( object ).text( i18n_val );
+				} );
+			} else {
+				throw new Error( 'Locale "' + locale + '" not supported.' );
+			}
+		} ).fail( function( error, errorMessage ) {
+			throw new Error( 'Error retrieving the i18n JSON file: ' + errorMessage + '.' );
+		} );
+
+	} else {
+		throw new Error( 'No i18n script object found.' );
+	}
+
+	var resolve = function( id ) {
+		return recursiveResolve( id, i18n ) || id;
+	};
+
+	var recursiveResolve = function( id, root ) {
+		if( !root ) {
+			return undefined;
+		}
+		var val = root[ id ];
+		if( !val && root ) {
+			if( id.indexOf( '.' ) > 1 ) {
+				var sp = id.split( '.' );
+				val = recursiveResolve( id.substring( id.indexOf( '.' ) + 1 ), root[ sp[ 0 ] ] );
+			}
+		}
+		return val;
+	}
+
+	return {
+		resolve: resolve
+	};
+} )();
